68f3e47d710cf34d400a9b0251a7bc7952ff3f13
Axel <Axel@LAPTOP-7JEOES6O>
2025-03-14 13:07:35 -0500

    Fundamental Race Object

---
+++ src/main/race/model/Race.java
@@ -0,0 +1,19 @@
+package src.main.race.model;
+import com.github.AxelPinard.PlayerCharacterSessionAid.stats.model.Size;
+
+public class Race {
+    private String raceName;
+    private Size SizeMod;
+    private int speed;
+
+    public Race() {}
+
+    public int getSpeed() {return speed;}
+    public void setSpeed(int speed) {this.speed = speed;}
+
+    public String getRaceName() {return raceName;}
+    public void setRaceName(String raceName) {this.raceName = raceName;}
+
+    public void setSizeMod(Size sizeMod) {SizeMod = sizeMod;}
+    public Size getSizeMod() {return SizeMod;}
+}
