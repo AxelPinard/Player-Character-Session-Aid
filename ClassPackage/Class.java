ca5859ccea32248272cbc1edc2a258fd7e4a1e9d
Axel <Axel@LAPTOP-7JEOES6O>
2025-03-14 12:11:41 -0500

    Foundational Class object

---
+++ ClassPackage/Class.java
@@ -0,0 +1,19 @@
+package ClassPackage;
+
+public class Class {
+
+    private String name;
+    private int  HitDie;
+    private int LvL;
+
+    public Class(){}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public int getHitDie() {return HitDie;}
+    public void setHitDie(int hitDie) {this.HitDie = hitDie;}
+
+    public int getLvL() {return LvL;}
+    public void setLvL(int lvL) {this.LvL = lvL;}
+}
